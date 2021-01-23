3bd547c8c2eae459353da97975b19b39e91db632
houda-rafrafi <hrafrafi1@gmail.com>
2021-01-23 19:10:31 +0100

    first commit

---
+++ src/main/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"								 
+	"gopdf-master"
+	"io/ioutil"
+)
+
+func main() {
+
+	var err error
+
+	files, err := ioutil.ReadDir(".")						//liste tout les fichiers du répertoire courant 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pdf := gopdf.GoPdf{}									//Création d'un fichier pdf
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4 }) 
+
+	for _, f := range files {								// parcourt les fichiers du répertoire courant et les mettre 
+		if f.Name() != "main.go" {	
+																
+			pdf.AddPage()									// insertions des images dans chaque page pdf 
+			pdf.Image(f.Name(), 0, 50, nil) 				
+		}
+	}
+
+	pdf.WritePdf("image.pdf")								//l'écriture dans le fichier pdf 
+}
